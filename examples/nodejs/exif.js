7ecb632ce33c166b7531e37fb6e276c80799a0a9
Brian Capouch <brianc@palaver.net>
2016-08-26 22:49:36 -0400

    Clean up and comment

--- examples/nodejs/exif.js
+++ examples/nodejs/exif.js
@@ -9,8 +9,8 @@ var jDataView = require('../../js/jdataview.js');
 var path = require('path');
 var fs = require('fs');
 
-// Quick and dirty--maybe replace later?
-// array = [{key:value},{key:value}]
+// Search for first record that has a given key
+//   Note: duplicates common
 function objectFindByKey(array, key) {
     for (var i = 0; i < array.length; i++) {
         if (array[i][key]) {
@@ -20,81 +20,57 @@ function objectFindByKey(array, key) {
     return null;
 }
 
-// If you don't want to use a wrapper like jDataView, you can wrap Node.js'
-// Buffer with the mandatory parts from the DataView interface.
-var DataView = (function() {
-	function DataView(_data) {
-		this._data = _data;
-		this.byteLength = this._data.length;
-	}
-	DataView.prototype.getUint8 = function(offset) {
-		return this._data.readUInt8(offset);
-	};
-	DataView.prototype.getUint16 = function(offset, littleEndian) {
-		if ((littleEndian != null) && littleEndian) {
-			return this._data.readUInt16LE(offset);
-		} else {
-			return this._data.readUInt16BE(offset);
-		}
-	};
-	DataView.prototype.getUint32 = function(offset, littleEndian) {
-		if ((littleEndian != null) && littleEndian) {
-			return this._data.readUInt32LE(offset);
-		} else {
-			return this._data.readUInt32BE(offset);
-		}
-	};
-	DataView.prototype.getInt32 = function(offset, littleEndian) {
-		if ((littleEndian != null) && littleEndian) {
-			return this._data.readInt32LE(offset);
-		} else {
-			return this._data.readInt32BE(offset);
-		}
-	};
-	return DataView;
-})();
-
-
+// Make sure user has given us an argument; no other error checking
 if (process.argv.length < 3) {
 	console.log('Usage: node ' + path.basename(__filename) + ' <filename>');
 	return;
 }
+
+// Set up variables
 var directory = process.argv[2];
 var lookup = [],
 	match = {},
   aid, coord;
 
 // This is the file that will allow us to "assign" images to asset records
-
-// Read in file contents as an array of strings
+//   Read in file contents as an array of strings
+//   aid = assetID, cart=Cartesian location
 var syncro = fs.readFileSync('aid-cart.txt').toString().split("\n");
 
 // Split each line into an element of the object array lookup
 for(i in syncro) {
+    // Vertical bar separates aid from cartCoord
     var pieces = syncro[i].split('|')
 
-		// Grrrr.  Hours to find this null line thing!!
+		// Don't blow up if we hit an empty line
 		if (typeof(pieces[1]) != 'string')
 		  break;
 
+    // Remove leading/trailing spaces
 		aid = pieces[0].trim()
 	  coord = pieces[1].trim()
+
+    // Assign asset ID to coordinate key
 		match[coord] = aid
 		// console.log(JSON.stringify(match))
-		// lookup[coord] = aid
+    // Add record to arrayl and clear record var
     lookup.push(match)
 		match = {}
 	}
 // console.log(JSON.stringify(lookup))
+
 // Get list of image files
 var files = fs.readdirSync(directory);
 
 // Generate a line of SQL for each file
-//   The "big trick" is that there can be multiple images for a marker
+//   Each marker can have muliple images
 files.forEach(function (path) {
+  // Filename without directory prefix
 	var justPath = path;
 	path = directory + path;
 	// console.log('Processing: ' + path);
+
+  // Process each image file
 	fs.readFile(path, function (err, data) {
 		if (err) {
 			console.log('Error reading file.');
@@ -105,20 +81,22 @@ files.forEach(function (path) {
 			var exif = new ExifReader();
 
 			// Parse the Exif tags.
-			// exif.load(data);
-			// Or, with jDataView you would use this:
 			exif.loadView(new jDataView(data));
 
-			// Output the tags.
+			// Output fields of interest
+      //  in this case ONLY our Sub-location
 			var tags = exif.getAllTags();
 			// console.log(justPath);
 			var index, subLocation;
 
+      // Process all tags for this image
 			for (name in tags) {
 			  // console.log(name + '*');tags[name].description
+
+        // We only want one field
 			  var pattern = /Sub-location/;
+        // On match, extract info and generate a line of SQL
 				if (pattern.test(name)) {
-				  // We're only interested in one IPTC tag, subLocation
 					subLocation = tags[name].description;
 					// console.log('Finding for ' + subLocation + ' in ' + justPath)
 					index = objectFindByKey(lookup, subLocation)
@@ -128,15 +106,15 @@ files.forEach(function (path) {
 						// console.log("Fetching location from " + index)
 						aid = lookup[index][subLocation]
 						// console.log('Got ' + aid)
-						// delete(lookup[subLocation])
 						lookup.splice(index, 1);
 
-					console.log("update asset set imagename = '" + justPath +
-					  "' where aid = '" + aid + "';");
-				  // console.log('Value of sublocation = ' + subLocation);
-			    console.log('\r')
-        }
+  					console.log("update asset set imagename = '" + justPath +
+  					  "' where aid = '" + aid + "';");
+  				  // console.log('Value of sublocation = ' + subLocation);
+  			    console.log('\r')
+          }
     else {
+      // Generate error message
       console.log('-- Error bad tag: ' + justPath + ' ' + subLocation)
       console.log()
     }
